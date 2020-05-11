0950e13309c41842ac32a1e8a14200e0bfce38b9
Demetrio Cruz <hesenrre@gmail.com>
2020-05-10 21:41:34 -0700

    base version

---
+++ lib/timecalculator.rb
@@ -0,0 +1,6 @@
+require "timecalculator/version"
+
+module Timecalculator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/timecalculator/version.rb
@@ -0,0 +1,3 @@
+module Timecalculator
+  VERSION = "0.1.0"
+end
